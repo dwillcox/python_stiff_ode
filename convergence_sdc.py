8c5af4485754d0ee14dcd566614262adc8b5a1ba
Michael Zingale <michael.zingale@stonybrook.edu>
2019-03-20 09:30:31 -0400

    add a report on convergence

--- convergence_sdc.py
+++ convergence_sdc.py
@@ -10,11 +10,11 @@ def doit():
 
     y_init = np.array([1.0, 0.0, 0.0])
 
-    tmax = 1.e2
+    tmax = 1.e4
 
     time = 0.0
 
-    nstep = [16, 32, 64, 128, 256, 512, 1024]
+    nstep = [16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
 
     y0_final = []
     y1_final = []
@@ -22,7 +22,7 @@ def doit():
 
     for ns in nstep:
         y_new, _ = sdc.sdc4(neq, time, tmax, tmax/ns,
-                            y_init, rhs, jac, tol=1.e-14)
+                            y_init, rhs, jac, tol=1.e-15)
 
         y0_final.append(y_new[0])
         y1_final.append(y_new[1])
@@ -32,9 +32,13 @@ def doit():
 
     # compute the difference between adjacent runs as the error
     err = []
+    diff = []
     for n in range(1, len(nstep)):
         err.append(abs(y0_final[n] - y0_final[n-1]))
-        print(err[-1])
+        diff.append("{} -> {}".format(nstep[n-1], nstep[n]))
+
+    for n in range(1, len(diff)):
+        print("{} to {}; order = {}".format(diff[n-1], diff[n], err[n-1]/err[n]))
 
 if __name__ == "__main__":
 
